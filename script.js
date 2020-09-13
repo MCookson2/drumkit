d72740f058effe156e301782c6cf3fca04d27c98
MCookson2 <matt.cookson2@gmail.com>
2020-09-13 18:11:49 +0100

    add code to listen to keyboard presses and play sound linked to specific key

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ keys.forEach(key => {
         }, 500)
     })
 })
+
+window.addEventListener('keydown', function(event){
+    var audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    var key = document.querySelector(`div[data-key="${event.keyCode}"`)
+    audio.play()
+    key.classList.add('playing')
+        setTimeout(function(){
+            key.classList.remove('playing')
+        }, 500)   
+})
