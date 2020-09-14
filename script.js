9231681a0896fd80b7d8a8b8eb56a6818e03cdd7
MCookson2 <matt.cookson2@gmail.com>
2020-09-14 17:15:55 +0100

    reformat code, create functions for forEach loops and change to transitionend rather than setTimeout

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-const keys = document.querySelectorAll('[data-key]')
+const keys = document.querySelectorAll('div[data-key]')
 
-keys.forEach(key => {
-    key.addEventListener('click', function(){
-        var dataSet = key.dataset.key;
-        document.querySelector(`audio[data-key="${dataSet}"]`).play();
-        key.classList.add('playing')
-        setTimeout(function(){
-            key.classList.remove('playing')
-        }, 500)
-    })
-})
+// moved function in forEach loop to its own function.
+function playSound(k){
+    var dataSet = k.dataset.key;
+    var audio = document.querySelector(`audio[data-key="${dataSet}"]`)
+    audio.currentTime = 0;
+    audio.play()
+
+    k.classList.add('playing')
+}
+
+// add function to remove class on event 'transitionend'
+function removeClass(k){
+    k.classList.remove('playing')
+}
+
+keys.forEach(key => key.addEventListener('click', function() {
+    playSound(key)
+}));
+
+keys.forEach(key => key.addEventListener('transitionend', function() {
+    removeClass(key)
+}));
 
 window.addEventListener('keydown', function(event){
     var audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
     var key = document.querySelector(`div[data-key="${event.keyCode}"`)
+    if (!audio) return;
+    
+    audio.currentTime = 0;
     audio.play()
+
     key.classList.add('playing')
-        setTimeout(function(){
-            key.classList.remove('playing')
-        }, 500)   
 })
